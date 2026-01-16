082768255ca202e0eb8324132d1d47a87c3961fd
UmerMalik298 <umer23672@gmail.com>
2026-01-16 19:21:46 +0500

    Add search and style

--- src/app/pages/carts/carts.ts
+++ src/app/pages/carts/carts.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Api } from '../../services/api';
 import { CommonModule } from '@angular/common';
 import { OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carts',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './carts.html',
   styleUrl: './carts.css',
 })
@@ -13,6 +15,16 @@ export class Carts implements OnInit {
   carts : any[] = [];
   loading = true;
   error = ''
+  searchText = '';
+
+  get filteredCarts() {
+     const q = this.searchText.toLowerCase();
+     if (!q) return this.carts;
+      return this.carts.filter((c: any) =>
+      String(c.id).toLowerCase().includes(q)
+    );
+
+  }
 
   constructor(private api : Api){}
 
@@ -25,7 +37,7 @@ export class Carts implements OnInit {
         this.loading = false;
       },
        error: () => {
-        this.error = 'Could not load products';
+       this.error = 'Could not load carts';   
         this.loading = false;
        }
     });
--- src/app/pages/orders/orders.ts
+++ src/app/pages/orders/orders.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Api } from '../../services/api';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './orders.html',
   styleUrl: './orders.css',
 })
@@ -13,6 +14,24 @@ export class Orders implements OnInit {
   users: any[] = [];
   loading = true;
   error = '';
+  searchText = '';
+
+  get filteredOrders() {
+    const q = this.searchText.trim().toLowerCase();
+    if (!q) return this.users;
+
+    return this.users.filter((u: any) =>{
+      const id = String(u.id ?? '').toLowerCase();
+      const first = String(u.firstName ?? '').toLowerCase();
+      const last = String(u.lastName ?? '').toLowerCase();
+      const email = String(u.email ?? '').toLowerCase();
+      return id.includes(q) 
+      || first.includes(q) 
+      || last.includes(q)
+       || email.includes(q);
+    }
+    );
+  }
 
   constructor(private api: Api) {}
 
